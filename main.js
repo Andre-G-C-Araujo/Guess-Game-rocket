4e563310b24a50055b406f68fb7d539337530615
AndreCaue <AndreCaue>
2023-08-15 12:52:56 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const screen1 = document.querySelector(".screenone");
+const screen2 = document.querySelector(".screentwo");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+let randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
+const handleTryClick = (event) => {
+  event.preventDefault();
+  const inputNumber = document.querySelector("#inputNumber");
+
+  if (inputNumber.value === "") {
+    alert("O input nao pode estar vazio, Abestaddoooooo");
+  }
+
+  if (Number(inputNumber.value) < 0 || Number(inputNumber.value) > 10) {
+    alert("Numero deve ser entre 0 e 10!! ABESTADOOOO");
+  }
+
+  if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen();
+    document.querySelector(
+      ".screentwo h2"
+    ).innerText = `Voce acertou em ${xAttempts} tentativas`;
+  }
+
+  inputNumber.value = "";
+  xAttempts++;
+};
+
+const handleResetClick = () => {
+  toggleScreen();
+  randomNumber = Math.round(Math.random() * 10);
+  xAttempts = 1;
+};
+
+const toggleScreen = () => {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+};
+
+const handleEnter = (e) => {
+  if (e.key === "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick();
+  }
+};
+
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener("keydown", handleEnter);
